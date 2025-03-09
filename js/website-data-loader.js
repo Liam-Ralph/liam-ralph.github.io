e33c6d93f8e016d37f74c0d33d3d919289ddbbad
Liam-Ralph <liamralph42@gmail.com>
2025-03-09 13:05:56 -0400

    add website data loading

---
+++ js/website-data-loader.js
@@ -0,0 +1,6 @@
+fetch("/data/website-data.txt")
+    .then((res) => res.text())
+    .then((text) => {
+        document.getElementById("first-header-version").innerHTML = "Version " + text.replace("Version: ", "");
+    })
+    .catch((e) => console.error(e));
