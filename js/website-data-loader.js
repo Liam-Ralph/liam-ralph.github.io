e958732aff476fe2ea8a859e1bf3dbcd9020a217
Liam-Ralph <liamralph42@gmail.com>
2025-07-31 18:19:10 -0400

    improve element loading time, remove unnecessary files

--- js/website-data-loader.js
+++
@@ -1,8 +0,0 @@
-// Loading Version Data
-
-fetch("/data/website-data.txt")
-    .then((res) => res.text())
-    .then((text) => {
-        document.getElementById("first-header-version").innerHTML = "Version " + text.replace("Version: ", "");
-    })
-    .catch((e) => console.error(e));
