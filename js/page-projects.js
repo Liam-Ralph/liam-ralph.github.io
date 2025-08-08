9f6f6dc708261e34aad6423bdae726aa14d0dc60
Liam Ralph <liamralph42@gmail.com>
2025-08-08 15:29:49 -0400

    add project link, title, and language tag

--- js/page-projects.js
+++ js/page-projects.js
@@ -11,6 +11,15 @@ for (let i in projects) {
     var projectBox = document.createElement("div");
     projectBox.className = "project-box";
 
+    // Project Link
+
+    var projectLink = document.createElement("a");
+    if (project.name === "Website") {
+        projectLink.href = "/projects/website";
+    } else {
+        projectLink.href = "/projects/" + project.pathName;
+    }
+
     // Project Logo
 
     var projectLogo = document.createElement("img");
@@ -22,9 +31,31 @@ for (let i in projects) {
     }
     projectLogo.alt = project.name + " Logo";
 
-    // Append Children
+    // Project Title
 
-    projectBox.appendChild(projectLogo);
+    var projectTitle = document.createElement("p");
+    projectTitle.className = "project-title offwhite-hover";
+    projectTitle.textContent = project.name;
+
+    projectLink.appendChild(projectLogo);
+    projectLink.appendChild(projectTitle);
+    projectBox.appendChild(projectLink);
+
+    // Project Tags
+
+    var projectTags = [];
+
+    var projectTagLang = document.createElement("p");
+    projectTagLang.className = "project-tag";
+    projectTagLang.textContent = project.languages[0].name;
+    projectTagLang.style.backgroundColor = project.languages[0].color;
+    projectTags.push(projectTagLang);
+
+    for (let i in projectTags) {
+        projectBox.appendChild(projectTags[i]);
+    }
+
+    // Append Children
     document.getElementById("section-recent").appendChild(projectBox);
     if (pinned_project_names.includes(project.name)) {
         document.getElementById("section-pinned").appendChild(projectBox.cloneNode(true));
