16147551ffe194f9cd8c93990ea7273a271ae6ac
Liam Ralph <liamralph@tutamail.com>
2025-08-10 13:31:00 -0400

    change highlight colour

--- js/page-about-me.js
+++ js/page-about-me.js
@@ -1,9 +1,9 @@
-import { languages, projects } from "./data-loader.js";
+import { projects } from "./data-loader.js";
 
 document.getElementById("projects-counter").textContent = projects.length + " Projects";
 
 var sum = 0;
-for (let i in languages) {
-    sum += languages[i].lines;
+for (let i in projects) {
+    sum += projects[i].lines;
 }
 document.getElementById("loc-counter").textContent = sum + " Lines of Code";
