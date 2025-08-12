3716ef2d3d892c8ed4cdd783df0d6053f7590e74
Liam Ralph <liamralph42@gmail.com>
2025-08-12 16:27:13 -0400

    add project stat tiles to statistics

--- js/page-statistics.js
+++ js/page-statistics.js
@@ -88,3 +88,66 @@ for (let i in projectsByLines) {
     barChart.appendChild(barsContainer);
 
 }
+
+// Project Tiles
+
+for (let i = projects.length - 1; i >= 0; i--) {
+
+    const project = projects[i];
+
+    // Project Box
+
+    var projectBox = document.createElement("div");
+    projectBox.className = "stats-box";
+
+    // Project Title
+    
+    var projectTitle = document.createElement("p");
+    projectTitle.className = "stats-title";
+    projectTitle.textContent = project.name;
+
+    projectBox.appendChild(projectTitle);
+
+    // Project Pie Chart
+
+    var projectPie = document.createElement("div");
+    projectPie.className = "stats-pie";
+    var backgroundImage = "conic-gradient(";
+    var currentPct = 0;
+    for (let ii in project.languages) {
+        const langPct = Math.round(project.linesList[ii] / project.lines * 100);
+        backgroundImage += project.languages[ii].color + " " + currentPct + "% ";
+        currentPct += langPct;
+        backgroundImage += currentPct + "%";
+        if (ii != project.languages.length - 1) {
+            backgroundImage += ", ";
+        }
+    }
+    backgroundImage += ")";
+    projectPie.style.backgroundImage = backgroundImage
+
+    projectBox.appendChild(projectPie);
+
+    // Project Pie Chart Legend
+
+    for (let ii in project.languages) {
+
+        const language = project.languages[ii];
+        
+        var projectLegend = document.createElement("p");
+        projectLegend.className = "stats-legend";
+        projectLegend.style.backgroundColor = language.color;
+        if (language.whiteText) {
+            projectLegend.style.color = "#C0C0C0"
+        }
+        projectLegend.textContent = language.name + ": " +
+            project.linesList[ii] + " LoC (" +
+            Math.round(project.linesList[ii] / project.lines * 100) + "%)";
+
+        projectBox.appendChild(projectLegend);
+
+    }
+
+    document.getElementById("projects-tile-container").appendChild(projectBox);
+
+}
