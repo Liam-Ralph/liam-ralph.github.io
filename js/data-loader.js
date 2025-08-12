ca7970dd87ef6ff523e0cc2e625c77b0b1d14717
Liam Ralph <liamralph42@gmail.com>
2025-08-12 14:47:11 -0400

    fix statistics for small screens, update metadata

--- js/data-loader.js
+++ js/data-loader.js
@@ -61,6 +61,7 @@ async function loadData() {
         "projects/archived/index.html", "projects/archived/styles.css",
         "projects/biomegen/index.html",
         "projects/pwrstat-gui/index.html",
+        "projects/website/index.html",
         "statistics/index.html", "statistics/styles.css"
     ]);
     var projects = [biomeGen, pwrStatGUI, website];
--- js/page-statistics.js
+++ js/page-statistics.js
@@ -9,7 +9,6 @@ for (let i in languages) {
     var legendItem = document.createElement("div");
     legendItem.className = "legend-item";
     legendItem.style.backgroundColor = language.color;
-    legendItem.style.width = (100 / languages.length) + "%";
     legendItem.textContent = language.name;
     if (language.whiteText) {
         legendItem.style.color = "#C0C0C0";
@@ -24,8 +23,8 @@ for (let i in languages) {
 var languagesByLines = languages.slice();
 
 for (let i = 0; i < languagesByLines.length - 1; i++) {
-    for (let ii = 0; ii < languagesByLines.length - i - 1; ii++){
-        if (languagesByLines[ii].lines > languagesByLines[ii + 1].lines){
+    for (let ii = 0; ii < languagesByLines.length - i - 1; ii++) {
+        if (languagesByLines[ii].lines > languagesByLines[ii + 1].lines) {
             const temp = languagesByLines[ii];
             languagesByLines[ii] = languagesByLines[ii + 1];
             languagesByLines[ii + 1] = temp;
@@ -36,8 +35,8 @@ for (let i = 0; i < languagesByLines.length - 1; i++) {
 var projectsByLines = projects.slice();
 
 for (let i = 0; i < projectsByLines.length - 1; i++) {
-    for (let ii = 0; ii < projectsByLines.length - i - 1; ii++){
-        if (projectsByLines[ii].lines < projectsByLines[ii + 1].lines){
+    for (let ii = 0; ii < projectsByLines.length - i - 1; ii++) {
+        if (projectsByLines[ii].lines < projectsByLines[ii + 1].lines) {
             const temp = projectsByLines[ii];
             projectsByLines[ii] = projectsByLines[ii + 1];
             projectsByLines[ii + 1] = temp;
@@ -63,14 +62,20 @@ for (let i in projectsByLines) {
         bar.className = "chart-bar";
         bar.style.width = (project.linesList[ii] * maxWidth) + "px";
         bar.style.backgroundColor = project.languages[ii].color;
-        bar.style.whiteSpace = "pre";
-        if (ii == 0){
-            bar.textContent = project.name + "\n\r" +
-                project.linesList[ii] + " LoC (" +
-                Math.round(project.linesList[ii] / project.lines * 100) + "%)";
+        if (window.innerWidth >= 1500) {
+            if (ii == 0) {
+                bar.textContent = project.name + "\n" +
+                    project.linesList[ii] + " LoC (" +
+                    Math.round(project.linesList[ii] / project.lines * 100) + "%)";
+            } else {
+                bar.textContent = "\n" + project.linesList[ii] + " LoC (" +
+                    Math.round(project.linesList[ii] / project.lines * 100) + "%)";
+            }
         } else {
-            bar.textContent = "\n\r" + project.linesList[ii] + " LoC (" +
-                Math.round(project.linesList[ii] / project.lines * 100) + "%)";
+            if (ii == 0) {
+                bar.textContent = "\n\r  " + project.name;
+                bar.style.textAlign = "left";
+            }
         }
         if (project.languages[ii].whiteText) {
             bar.style.color = "#C0C0C0";
