0afc7e7dacd1b358758069ed863d6712ba9f4d8e
Liam Ralph <liamralph42@gmail.com>
2025-11-07 21:03:41 -0500

    add persistent cookies (unstable)

--- cookie-reader.js
+++ cookie-reader.js
@@ -1,6 +1,14 @@
-function cookieReader(name) {
+export function readCookie(name) {
     let cookies = document.cookie.split("; ");
-    cookies.forEach(function(cookie) {
-        
-    });
+    for (let i in cookies) {
+        const cookie = cookies[i];
+        if (cookie.startsWith(name + "=")) {
+            const cookieValue = cookie.replace(name + "=", "").trim();
+            if (cookieValue.length < 100) {
+                console.log("Cookie " + name + ": " + cookieValue);
+            }
+            return cookieValue;
+        }
+    }
+    return "";
 }
