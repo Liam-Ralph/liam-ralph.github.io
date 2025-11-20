37375ef2740d536cbfed2a9b33cb67ae2c031586
Liam Ralph <liamralph@tutamail.com>
2025-11-20 11:41:27 -0500

    remove c-rewrite progress bar

--- projects/biomegen/script.js
+++ projects/biomegen/script.js
@@ -2,10 +2,6 @@
 
 const startTime = new Date();
 
-// Import From Data Loader
-
-import { projects } from "/data-loader.js";
-
 // Test Result Class
 
 class TestResult {
@@ -22,30 +18,6 @@ class TestResult {
         this.pct95 = pct95;
     }
 }
-
-
-// C Rerwite Progress Bar
-
-// Getting Number of Lines Left
-
-let urlName =
-    "https://raw.githubusercontent.com/Liam-Ralph/biomegen/refs/heads/c-rewrite/main_left.txt";
-let response = await fetch(urlName);
-const fileText = await response.text();
-const linesLeft = fileText.split("\n").length;
-
-// Calculating Progress Percentage
-
-const linesTotal = projects[0].linesList[0];
-const progress = (linesTotal - linesLeft) / linesTotal;
-
-// Updating Progress bar
-
-let barInner = document.getElementById("c-rewrite-progress-bar-inner");
-barInner.textContent =
-    "C Rewrite Progress: " + Math.round(progress * 1000) / 10 + "%";
-barInner.style.width =
-    (document.getElementById("c-rewrite-progress-bar").offsetWidth * progress - 20) + "px";
 
 
 // Statistics
